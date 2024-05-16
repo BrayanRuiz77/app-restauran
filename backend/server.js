5339c5921599ff0c667138adc587e8d23ad0e20b
BrayanRuiz77 <andresbrayanpe@gmail.com>
2024-05-15 20:03:57 -0500

    UPDATE

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
-
+const mysql = require("mysql")
 
 const app = express();
 const PORT = 3000;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "login_restaurante"
+})
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos MySQL establecida');
+});
 
 app.get('/', (req, res) => {
     res.send('Hola, mundo desde Express jorge!');
