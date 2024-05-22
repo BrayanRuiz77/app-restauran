f38e2cc79081d37b8f34d68a0aae2d97e0bcbe4f
Jhair7474 <marlonmoncayo7474@gmail.com>
2024-05-22 16:24:47 -0500

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,26 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactDOM from 'react-dom';
 
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
+// Crea el root usando el método createRoot de ReactDOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderiza la aplicación
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Medir el rendimiento de la aplicación
 reportWebVitals();
+
